94e66ec9ae4cd19017a938478ba06c141253e7a9
Dzmitry Antsybor <Dzmitry_Antsybor@discovery.com>
2021-02-24 11:37:12 +0300

    - removing of the files from home task №2

--- src/utilities/textControlUtilities.js
+++
@@ -1,9 +0,0 @@
-export const colors = ['white', 'blue', 'green', 'yellow', 'red'];
-
-export function calculateCounterState(state, counterLimit) {
-  const { textDisplay, counter } = state;
-  if (!textDisplay) {
-    return counter;
-  }
-  return counter < counterLimit ? counter + 1 : 0;
-}

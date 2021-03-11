e9e182ad9e0b532311bc5ef61dc2a50ba0256456
Dzmitry Antsybor <Dzmitry_Antsybor@discovery.com>
2021-03-11 18:50:14 +0300

    - removed highlighting of selected item functionality

--- src/components/navigation/dropDown.js
+++ src/components/navigation/dropDown.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/navigation/dropDown.scss';
 
@@ -7,29 +7,11 @@ export default function dropDown(props) {
   const [openDropDownState, setOpenDropDownState] = useState('');
   const [selectedValue, setSelectedValue] = useState('');
 
-  const generateOptionsState = (items, target) => items.map(
-    (item) => {
-      let selected = '';
-      if (item === target?.textContent) {
-        selected = 'selected';
-      }
-      return (
-        <span key={item} className={`custom-option ${selected}`} data-value={item} onClick={chooseElementEvent}>
-          {item}
-        </span>
-      );
-    },
-  );
-
   const chooseElementEvent = (e) => {
     const { target } = e;
-    const newOptionsState = generateOptionsState(options, target);
     setOpenDropDownState('');
-    setDropDownOptionsState(newOptionsState);
     setSelectedValue(target.textContent);
   };
-
-  const [dropDownOptionsState, setDropDownOptionsState] = useState(generateOptionsState(options));
   const openMenuEvent = () => { setOpenDropDownState('open'); };
   const handleClick = (e) => {
     if (!e.target.classList.contains('custom-select__trigger')
@@ -45,6 +27,18 @@ export default function dropDown(props) {
     };
   }, []);
 
+  const optionsBuilder = (item) => (
+    <span
+      key={item}
+      className="custom-option"
+      data-value={item}
+      onClick={chooseElementEvent}
+    >
+      {item}
+    </span>
+  );
+
+  const optionItems = useMemo(() => options.map(optionsBuilder), [options]);
   return (
     <div className="custom-select-wrapper">
       <div className={`custom-select ${openDropDownState}`} onClick={openMenuEvent}>
@@ -53,7 +47,7 @@ export default function dropDown(props) {
           <div className="arrow" />
         </div>
         <div className="custom-options">
-          {dropDownOptionsState}
+          {optionItems}
         </div>
       </div>
     </div>

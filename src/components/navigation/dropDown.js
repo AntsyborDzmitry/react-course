4692632742aa5f68f9587c473538bb7b97c13590
Dzmitry Antsybor <Dzmitry_Antsybor@discovery.com>
2021-03-11 19:28:35 +0300

    - to handle dropdown open/close state has been replaced DOM approach to useState - removed highlighting of selected item functionality

--- src/components/navigation/dropDown.js
+++ src/components/navigation/dropDown.js
@@ -1,62 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/navigation/dropDown.scss';
 
 export default function dropDown(props) {
   const { options, sortMovieHandler } = props;
+  const [openDropDownState, setOpenDropDownState] = useState('');
+  const [selectedValue, setSelectedValue] = useState('');
 
   const chooseElementEvent = (e) => {
     const { target } = e;
-    if (!target.classList.contains('selected')) {
-      const selected = target.parentNode.querySelector('.custom-option.selected');
-      if (selected) {
-        selected.classList.remove('selected');
-      }
-      target.classList.add('selected');
-      target.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = target.textContent;
-      sortMovieHandler(target.dataset.value);
-    }
+    setOpenDropDownState('');
+    setSelectedValue(target.textContent);
+    sortMovieHandler(target.dataset.value);
   };
-
-  const openMenuEvent = (e) => { e.currentTarget.classList.toggle('open'); };
+  const openMenuEvent = () => { setOpenDropDownState('open'); };
   const handleClick = (e) => {
-    const select = document.querySelector('.custom-select');
-    if (!select.contains(e.target)) {
-      select.classList.remove('open');
+    if (!e.target.classList.contains('custom-select__trigger')
+      && !e.target.classList.contains('selected-value')) {
+      setOpenDropDownState('');
     }
-  };
-
-  const buildSortOption = (item, ind) => {
-    const status = ind === 0 ? 'selected' : '';
-    return (
-      <span
-        key={item.key}
-        className={`custom-option ${status}`}
-        data-value={item.key}
-        onClick={chooseElementEvent}
-      >
-        {item.value}
-      </span>
-    );
   };
 
   useEffect(() => {
+    setSelectedValue(options[0].value);
     document.addEventListener('click', handleClick);
     return () => {
       document.removeEventListener('click', handleClick);
     };
   }, []);
+
+  const optionsBuilder = (item) => (
+    <span
+      key={item.key}
+      className="custom-option"
+      data-value={item.key}
+      onClick={chooseElementEvent}
+    >
+      {item.value}
+    </span>
+  );
+
+  const optionItems = useMemo(() => options.map(optionsBuilder), [options]);
   return (
     <div className="custom-select-wrapper">
-      <div className="custom-select" onClick={openMenuEvent}>
+      <div className={`custom-select ${openDropDownState}`} onClick={openMenuEvent}>
         <div className="custom-select__trigger">
-          <span>{options[0].value}</span>
+          <span className="selected-value">{selectedValue}</span>
           <div className="arrow" />
         </div>
         <div className="custom-options">
-          {
-            options.map((item, ind) => buildSortOption(item, ind))
-          }
+          {optionItems}
         </div>
       </div>
     </div>
